db0c860e5cfcaf356f75c3aa8ab490cf9ac15aac
Shiv Prasad <shivprasadvairal82@gmail.com>
2025-01-12 19:09:50 +0530

    initial commit

---
+++ file15.js
@@ -0,0 +1,19 @@
+// ternary operator
+
+// let age = 4;
+// let drink;
+
+// if(age>=5){
+//     drink = "coffee";
+// }else{
+//     drink = "milk";
+// }
+
+// console.log(drink);\
+
+// ~you dont need to write these type of big code you should use the TERNARY OPERATOR it also called as a CONDITIONAL OPERATOR...
+
+
+let age =4;
+let drink = age >= 5 ? "coffee" : "milk";
+console.log(drink);
