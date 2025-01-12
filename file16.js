ea6d2545758aff80dd61185519d2412a64aefc72
Shiv Prasad <shivprasadvairal82@gmail.com>
2025-01-12 22:02:21 +0530

    initial commit

---
+++ file16.js
@@ -0,0 +1,29 @@
+// and,or operator 
+
+let firstName = "Shivprasad";
+let age = 19;
+
+// if(firstName[0] === "s"){
+//     console.log("your name start with s")
+// }
+
+// if(age>18){
+//     console.log("you are above 18");
+// }
+
+// to check both of these conditions we can use AND operator
+// and && Operator
+
+// and (&&) operator ~
+// if(firstName[0] === "S" && age >18){
+//     console.log("name starts with S and above 18");
+// }else{
+//     console.log("inside else");
+// }
+
+// or (||) operator ~
+if(firstName[0] === "S" || age >18){
+    console.log("inside if");
+}else{
+    console.log("inside else");
+}
