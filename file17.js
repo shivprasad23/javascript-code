173f0d8b6fb41da0abfbfd9c630e06b6c094bec3
Shiv Prasad <shivprasadvairal82@gmail.com>
2025-01-14 23:38:00 +0530

    file18

--- file17.js
+++ file17.js
@@ -5,3 +5,7 @@
 // 18 your guess is right
 // 15 too low
 // 21  too high 
+
+let winningNumber =19;
+let userGuess = promote("Guess a number");
+console.log(typeof userGuess,userGuess);
---
+++ file18.js
@@ -0,0 +1,18 @@
+// if 
+// else if 
+// else if 
+// else if 
+// else
+
+let tempinDegree = 50;
+
+if(tempinDegree < 0){
+    console.log("extremely cold outside");
+}else if(tempinDegree < 16){
+    console.log("it is cold outside");
+}else if(tempinDegree < 25){
+    console.log("weather is okay");
+}else {
+    console.log("it is warm outside");
+}
+
