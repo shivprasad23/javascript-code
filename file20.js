aa9e6af35ef1dd5bb7d7096290ed8baec979c003
Shiv Prasad <shivprasadvairal82@gmail.com>
2025-01-15 00:37:14 +0530

    file20.js

---
+++ file20.js
@@ -0,0 +1,34 @@
+// while loop 
+
+// this process is known as 'dry' it means dont repeat yourself
+
+// let i=0;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+
+let i=0;
+
+while(i<=3){
+    console.log(i);
+    i++;
+}
+console.log(`current value of i is ${i}`);
