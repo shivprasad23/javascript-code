173ebd3342f0cef1105e15135fa5beb4a9381b64
Shiv Prasad <shivprasadvairal82@gmail.com>
2025-01-21 10:21:38 +0530

    file26.js

--- file26.js
+++ file26.js
@@ -1,9 +1,25 @@
 // intro to arrays ~reference type , collection of elements / ordered collection of items
+// refrance type
+// how to create arrays'
 
+// ordered collection of item
 
-let fruits =["apple","mango","grapes"];
-console.log(fruits[0]);
-// how to create arrays
+// let fruits =["apple","mango","grapes"];
+// let numbers = [1,2,3,4];
+// let mixed = [1,2,2,3,"string",null,undefined];
+// console.log(mixed);
+// console.log(fruits[2]);
+// console.log(numbers[0]);
+let fruits = ["apple","mango","grapes"]
+let obj = {}; //object literal
+// console.log(fruits);
+// fruits[1] = "banana";
+// console.log(fruits);
+console.log(typeof fruits);
+console.log(typeof obj);
+console.log(Array.isArray(fruits));
+console.log(Array.isArray(obj));
+
 
 // array indexing
 
