f6c7cd92750a5efd5d12c748bab91e1f29ddd8e7
Shiv Prasad <shivprasadvairal82@gmail.com>
2025-01-22 23:31:26 +0530

    file27.js

---
+++ file27.js
@@ -0,0 +1,23 @@
+// array push loop
+
+// array shift unshift
+
+let fruits = ["apple","mango","grapes"];
+
+// push
+// fruits.push("banana");
+// console.log(fruits);
+// let poppedFruit = fruits.pop();
+// console.log(fruits);
+// console.log("popped fruits is",poppedFruit);
+
+// unshift
+// fruits.unshift("banana");
+// fruits.unshift("myFruit");
+// console.log(fruits);
+
+
+// shift
+fruits.shift();
+console.log(fruits);
+console.log("removed fruit is",removedFruit);
