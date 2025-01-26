c1f7ee6988b6cbd1dd35345342ccb1c7633e33bf
Shiv Prasad <shivprasadvairal82@gmail.com>
2025-01-26 22:50:16 +0530

    file30.js

---
+++ file30.js
@@ -0,0 +1,14 @@
+// for loop in array 
+
+let fruits = ["apple","mango","grapes","banana"];
+
+// for(let i = 0; i<=9; i++){
+//     console.log(i);
+// }
+
+// consolwe.log(fruits.length);
+// console.log(fruits[fruits.length-2]);
+for(let i=0; i<=fruits.length-1; i++){
+   fruits2.push(fruits[i].toUpperCase());
+}
+console.log(fruits2);
